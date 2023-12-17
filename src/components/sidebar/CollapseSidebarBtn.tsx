95fc95536e85a7d757828d19f4d02918ba326b1b
dLevz <100078678+danlevison@users.noreply.github.com>
2023-12-17 12:28:44 +0000

    style: remove initial sidebar animation on page load

---
+++ src/components/sidebar/CollapseSidebarBtn.tsx
@@ -0,0 +1,45 @@
+import { useDispatch } from "react-redux"
+import { setSidebarClose, setSidebarOpen } from "@/redux/features/sidebarSlice"
+
+export default function CollapseSidebarBtn({
+	sidebarStatus
+}: {
+	sidebarStatus: boolean
+}) {
+	const dispatch = useDispatch()
+	return (
+		<button
+			onClick={
+				sidebarStatus
+					? () => dispatch(setSidebarClose())
+					: () => dispatch(setSidebarOpen())
+			}
+			className={`hidden transition-all duration-300 md:mt-auto md:gap-2 ${
+				sidebarStatus ? "md:flex md:items-center" : "md:inline-block"
+			} `}
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="none"
+				className={`h-6 w-6 fill-secondary transition-all duration-300 ${
+					sidebarStatus && "rotate-180"
+				}`}
+			>
+				<path
+					fillRule="evenodd"
+					d="M4.72 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 010-1.06zm6 0a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 010-1.06z"
+					clipRule="evenodd"
+				/>
+			</svg>
+
+			<span
+				className={`hidden text-body-1 text-primaryText ${
+					sidebarStatus ? "md:inline-block" : "md:hidden"
+				}`}
+			>
+				Collapse
+			</span>
+		</button>
+	)
+}
--- src/components/sidebar/Nav.tsx
+++ src/components/sidebar/Nav.tsx
@@ -1,24 +1,21 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import Link from "next/link"
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
 import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
-import { setSidebarClose, setSidebarOpen } from "@/redux/features/sidebarSlice"
+import { setSidebarClose } from "@/redux/features/sidebarSlice"
 import { RootState } from "@/redux/store"
-import { pageRoutes } from "@/lib/routes"
-import { usePathname } from "next/navigation"
 import Logo from "./Logo"
 import SocialLinks from "./SocialLinks"
 import NavLinks from "./NavLinks"
+import CollapseSidebarBtn from "./CollapseSidebarBtn"
 
 export default function Nav() {
 	const sidebarStatus = useSelector(
 		(state: RootState) => state.sidebar.sidebarStatus
 	)
 	const dispatch = useDispatch()
-	const pathname = usePathname()
 	const [windowWidth, setWindowWidth] = useState(
 		typeof window !== "undefined" ? window.innerWidth : null
 	)
@@ -49,7 +46,7 @@ export default function Nav() {
 	return (
 		<motion.nav
 			layout
-			initial="initial"
+			initial={false}
 			animate={{
 				width: getNavWidth(),
 				transition: {
@@ -68,40 +65,7 @@ export default function Nav() {
 
 			<SocialLinks sidebarStatus={sidebarStatus} />
 
-			<button
-				type="button"
-				onClick={
-					sidebarStatus
-						? () => dispatch(setSidebarClose())
-						: () => dispatch(setSidebarOpen())
-				}
-				className={`hidden transition-all duration-300 md:mt-auto md:gap-2 ${
-					sidebarStatus ? "md:flex md:items-center" : "md:inline-block"
-				} `}
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="0 0 24 24"
-					fill="none"
-					className={`h-6 w-6 fill-secondary transition-all duration-300 ${
-						sidebarStatus && "rotate-180"
-					}`}
-				>
-					<path
-						fillRule="evenodd"
-						d="M4.72 3.97a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 01-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 010-1.06zm6 0a.75.75 0 011.06 0l7.5 7.5a.75.75 0 010 1.06l-7.5 7.5a.75.75 0 11-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 010-1.06z"
-						clipRule="evenodd"
-					/>
-				</svg>
-
-				<span
-					className={`hidden text-body-1 text-white ${
-						sidebarStatus ? "md:inline-block" : "md:hidden"
-					}`}
-				>
-					Collapse
-				</span>
-			</button>
+			<CollapseSidebarBtn sidebarStatus={sidebarStatus} />
 		</motion.nav>
 	)
 }
