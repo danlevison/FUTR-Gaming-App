62c949646186830e0217d0807d7d6e045fd5be26
dLevz <100078678+danlevison@users.noreply.github.com>
2024-02-14 10:45:58 +0000

    style: Reduce game list grid cols at xl screen sizes + update scrollbar styles

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -1,24 +1,20 @@
-import { GameT } from "@/types"
-import GameCard from "./gameCard/GameCard"
+import { GameT } from "@/types";
+import GameCard from "./gameCard/GameCard";
 
 type GamesListProps = {
-	games: GameT[]
-	sliceValue?: number
-	ownerId?: string
-}
+  games: GameT[];
+  sliceValue?: number;
+  ownerId?: string;
+};
 
 export default function GamesList({ games, ownerId }: GamesListProps) {
-	return (
-		<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 w-full">
-			{games?.map((game) => (
-				<li key={game.id}>
-					<GameCard
-						key={game.id}
-						game={game}
-						ownerId={ownerId}
-					/>
-				</li>
-			))}
-		</ul>
-	)
+  return (
+    <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-10 w-full">
+      {games?.map((game) => (
+        <li key={game.id}>
+          <GameCard key={game.id} game={game} ownerId={ownerId} />
+        </li>
+      ))}
+    </ul>
+  );
 }
