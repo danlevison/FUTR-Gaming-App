e35b9500931920b617ce772806d8057092f44be4
dLevz <100078678+danlevison@users.noreply.github.com>
2024-03-04 17:37:03 +0000

    styles: Add active styles to pagination to highlight current page to user

--- src/app/wishlist/[wishlist_id]/components/WishlistGames.tsx
+++ src/app/wishlist/[wishlist_id]/components/WishlistGames.tsx
@@ -1,62 +1,59 @@
-"use client"
+"use client";
 
-import Link from "next/link"
-import { useParams } from "next/navigation"
-import { useFetchWishlistQuery } from "@/redux/features/wishlistApiSlice"
-import { useFetchUsersQuery } from "@/redux/features/usersApiSlice"
-import LoadingGames from "@/components/loading/LoadingGames"
-import GamesList from "@/components/GamesList"
-import ErrorDisplay from "@/components/ErrorDisplay"
-import useUser from "@/hooks/useUser"
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { useFetchWishlistQuery } from "@/redux/features/wishlistApiSlice";
+import { useFetchUsersQuery } from "@/redux/features/usersApiSlice";
+import LoadingGames from "@/components/loading/LoadingGames";
+import GamesList from "@/components/GamesList";
+import ErrorDisplay from "@/components/ErrorDisplay";
+import useUser from "@/hooks/useUser";
 
 export default function WishListGames() {
-	const user = useUser()
-	const { wishlist_id } = useParams()
-	const {
-		data: wishlistData,
-		isLoading,
-		isFetching,
-		isError
-	} = useFetchWishlistQuery(wishlist_id as string)
-	const { data: userData } = useFetchUsersQuery({})
+  const user = useUser();
+  const { wishlist_id } = useParams();
+  const {
+    data: wishlistData,
+    isLoading,
+    isFetching,
+    isError,
+  } = useFetchWishlistQuery(wishlist_id as string);
+  const { data: userData } = useFetchUsersQuery({});
 
-	const userDetails = () => {
-		return userData?.find((fetchedUser) => fetchedUser?.uid === wishlist_id)
-	}
+  const userDetails = () => {
+    return userData?.find((fetchedUser) => fetchedUser?.uid === wishlist_id);
+  };
 
-	const { displayName } = userDetails() || {}
+  const { displayName } = userDetails() || {};
 
-	return (
-		<>
-			{displayName && (
-				<h2 className="text-2xl sm:text-3xl mb-2">
-					{user?.uid === wishlist_id ? (
-						"Your"
-					) : (
-						<Link
-							href={`/user/${wishlist_id}`}
-							className="underline"
-						>
-							{displayName}&apos;s
-						</Link>
-					)}{" "}
-					Wishlist
-				</h2>
-			)}
-			<div className="flex flex-col items-center mt-5">
-				{(isLoading || isFetching) && <LoadingGames />}
+  return (
+    <>
+      {displayName && (
+        <h2 className="text-2xl sm:text-3xl mb-2">
+          {user?.uid === wishlist_id ? (
+            "Your"
+          ) : (
+            <Link href={`/user/${wishlist_id}`} className="underline">
+              {displayName}&apos;s
+            </Link>
+          )}{" "}
+          Wishlist
+        </h2>
+      )}
+      <div className="flex flex-col items-center mt-5">
+        {(isLoading || isFetching) && <LoadingGames />}
 
-				{wishlistData && wishlistData.games?.length > 0 && (
-					<>
-						<GamesList
-							games={wishlistData.games}
-							ownerId={wishlistData.ownerId}
-						/>
-					</>
-				)}
+        {wishlistData && wishlistData.games?.length > 0 && (
+          <>
+            <GamesList
+              games={wishlistData.games}
+              ownerId={wishlistData.ownerId}
+            />
+          </>
+        )}
 
-				{isError && <ErrorDisplay errorMessage="Wishlist is empty." />}
-			</div>
-		</>
-	)
+        {isError && <ErrorDisplay errorMessage="Wishlist is empty." />}
+      </div>
+    </>
+  );
 }
--- src/components/Pagnination.tsx
+++ src/components/Pagnination.tsx
@@ -1,96 +1,100 @@
 import {
-	MdOutlineKeyboardArrowRight,
-	MdOutlineKeyboardArrowLeft
-} from "react-icons/md"
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardArrowLeft,
+} from "react-icons/md";
 import {
-	Pagination,
-	PaginationContent,
-	PaginationEllipsis,
-	PaginationItem
-} from "@/components/ui/pagination"
-import { Button } from "./ui/button"
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+} from "@/components/ui/pagination";
+import { Button } from "./ui/button";
 
 type PaginationT = {
-	pageHandler: (pageValue: number) => void
-	nextPage: string | null
-	prevPage: string | null
-	currentPage: number
-	count: number
-}
+  pageHandler: (pageValue: number) => void;
+  nextPage: string | null;
+  prevPage: string | null;
+  currentPage: number;
+  count: number;
+};
 
 export default function PaginationComponent({
-	pageHandler,
-	nextPage,
-	prevPage,
-	currentPage,
-	count
+  pageHandler,
+  nextPage,
+  prevPage,
+  currentPage,
+  count,
 }: PaginationT) {
-	const pageNextHandler = () => {
-		if (nextPage !== null) pageHandler(++currentPage)
-	}
+  const pageNextHandler = () => {
+    if (nextPage !== null) pageHandler(++currentPage);
+  };
 
-	const pagePrevHandler = () => {
-		if (prevPage !== null) pageHandler(--currentPage)
-	}
+  const pagePrevHandler = () => {
+    if (prevPage !== null) pageHandler(--currentPage);
+  };
 
-	const pageNumberHandler = (pageNumber: number) => {
-		pageHandler(pageNumber)
-	}
+  const pageNumberHandler = (pageNumber: number) => {
+    pageHandler(pageNumber);
+  };
 
-	return (
-		<Pagination className="mt-10">
-			<PaginationContent className="flex sm:gap-2">
-				<PaginationItem>
-					<Button
-						onClick={pagePrevHandler}
-						variant={"outline"}
-						disabled={prevPage === null ? true : false}
-					>
-						<MdOutlineKeyboardArrowLeft size={25} /> Previous
-					</Button>
-				</PaginationItem>
-				<PaginationItem>
-					<Button
-						onClick={() => pageNumberHandler(1)}
-						variant={"outline"}
-						className="hidden sm:block"
-					>
-						1
-					</Button>
-				</PaginationItem>
-				<PaginationItem>
-					<Button
-						onClick={() => pageNumberHandler(2)}
-						variant={"outline"}
-						className="hidden sm:block"
-						disabled={count < 40}
-					>
-						2
-					</Button>
-				</PaginationItem>
-				<PaginationItem>
-					<Button
-						onClick={() => pageNumberHandler(3)}
-						variant={"outline"}
-						className="hidden sm:block"
-						disabled={count < 80}
-					>
-						3
-					</Button>
-				</PaginationItem>
-				<PaginationItem>
-					<PaginationEllipsis className="hidden sm:flex" />
-				</PaginationItem>
-				<PaginationItem>
-					<Button
-						onClick={pageNextHandler}
-						variant={"outline"}
-						disabled={nextPage === null ? true : false}
-					>
-						Next <MdOutlineKeyboardArrowRight size={25} />
-					</Button>
-				</PaginationItem>
-			</PaginationContent>
-		</Pagination>
-	)
+  const handleActiveStyles = (pageNumber: number) => {
+    return currentPage === pageNumber ? "bg-white text-background" : "";
+  };
+
+  return (
+    <Pagination className="mt-10">
+      <PaginationContent className="flex sm:gap-2">
+        <PaginationItem>
+          <Button
+            onClick={pagePrevHandler}
+            variant={"outline"}
+            disabled={prevPage === null ? true : false}
+          >
+            <MdOutlineKeyboardArrowLeft size={25} /> Previous
+          </Button>
+        </PaginationItem>
+        <PaginationItem>
+          <Button
+            onClick={() => pageNumberHandler(1)}
+            variant={"outline"}
+            className={`hidden sm:block ${handleActiveStyles(1)}`}
+          >
+            1
+          </Button>
+        </PaginationItem>
+        <PaginationItem>
+          <Button
+            onClick={() => pageNumberHandler(2)}
+            variant={"outline"}
+            className={`hidden sm:block ${handleActiveStyles(2)}`}
+            disabled={count < 40}
+          >
+            2
+          </Button>
+        </PaginationItem>
+        <PaginationItem>
+          <Button
+            onClick={() => pageNumberHandler(3)}
+            variant={"outline"}
+            className={`hidden sm:block ${handleActiveStyles(3)}`}
+            disabled={count < 80}
+          >
+            3
+          </Button>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationEllipsis className="hidden sm:flex" />
+        </PaginationItem>
+        <PaginationItem>
+          <Button
+            onClick={pageNextHandler}
+            variant={"outline"}
+            disabled={nextPage === null ? true : false}
+          >
+            Next <MdOutlineKeyboardArrowRight size={25} />
+          </Button>
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
 }
