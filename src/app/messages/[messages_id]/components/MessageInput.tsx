e2e3da48207e4a2580b09185d8d6a53905e36c14
dLevz <100078678+danlevison@users.noreply.github.com>
2024-03-06 17:22:50 +0000

    feat: Implement ability to send message using enter key to improve UX

--- src/app/messages/[messages_id]/components/MessageInput.tsx
+++ src/app/messages/[messages_id]/components/MessageInput.tsx
@@ -5,57 +5,59 @@ import { useSendMessageMutation } from "@/redux/features/messagesApiSlice"
 import useUser from "@/hooks/useUser"
 
 type MessageInputProps = {
-	selectedUser: {
-		chatId: string
-		displayName: string
-		avatar: string
-		userId: string
-	}
+  selectedUser: {
+    chatId: string
+    displayName: string
+    avatar: string
+    userId: string
+  }
 }
 
 export default function MessageInput({ selectedUser }: MessageInputProps) {
-	const user = useUser()
-	const [text, setText] = useState("")
-	const { toast } = useToast()
-	const [sendMessage] = useSendMessageMutation()
+  const user = useUser()
+  const [text, setText] = useState("")
+  const { toast } = useToast()
+  const [sendMessage] = useSendMessageMutation()
 
-	const handleSendMessage = async () => {
-		try {
-			await sendMessage({
-				userId: user?.uid as string,
-				chatId: selectedUser.chatId,
-				messagedUserId: selectedUser.userId,
-				displayName: user?.displayName as string,
-				avatar: user?.avatar as string,
-				text: text
-			})
-			setText("")
-		} catch (error) {
-			toast({
-				variant: "destructive",
-				description: "Failed to send message, please try again."
-			})
-			console.error(error)
-		}
-	}
+  const handleSendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setText("")
 
-	return (
-		<div className="flex items-center">
-			<input
-				onChange={(e) => setText(e.target.value)}
-				value={text}
-				type="text"
-				placeholder="Type something..."
-				className="w-full h-14 md:rounded-bl-md border-none outline-none p-3 bg-blue-950"
-			/>
-			<Button
-				onClick={handleSendMessage}
-				variant={"outline"}
-				disabled={!text}
-				className="h-14 rounded-none md:rounded-br-md border-none"
-			>
-				Send
-			</Button>
-		</div>
-	)
+    try {
+      await sendMessage({
+        userId: user?.uid as string,
+        chatId: selectedUser.chatId,
+        messagedUserId: selectedUser.userId,
+        displayName: user?.displayName as string,
+        avatar: user?.avatar as string,
+        text: text,
+      })
+      setText("")
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        description: "Failed to send message, please try again.",
+      })
+      console.error(error)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSendMessage} className="flex items-center">
+      <input
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+        type="text"
+        placeholder="Type something..."
+        className="w-full h-14 md:rounded-bl-md border-none outline-none p-3 bg-blue-950"
+      />
+      <Button
+        variant={"outline"}
+        disabled={!text}
+        className="h-14 rounded-none md:rounded-br-md border-none"
+      >
+        Send
+      </Button>
+    </form>
+  )
 }
