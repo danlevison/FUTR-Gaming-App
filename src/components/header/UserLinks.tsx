afc3a63f3d51629aefe590647d19740520a1df48
dLevz <100078678+danlevison@users.noreply.github.com>
2024-03-11 14:29:32 +0000

    fix: Update next Link href to ensure client-side routing

--- src/components/header/UserLinks.tsx
+++ src/components/header/UserLinks.tsx
@@ -4,56 +4,54 @@ import { userRoutes } from "@/routes/routes"
 import useUser from "@/hooks/useUser"
 
 export default function UserLinks({
-	sidebarStatus,
-	nav,
-	handleNav
+  sidebarStatus,
+  nav,
+  handleNav,
 }: {
-	sidebarStatus?: boolean
-	nav?: boolean
-	handleNav?: () => void
+  sidebarStatus?: boolean
+  nav?: boolean
+  handleNav?: () => void
 }) {
-	const user = useUser()
-	const pathname = usePathname()
-	return (
-		<ul className="flex flex-col gap-5 mt-5 md:mt-8">
-			{userRoutes.map(({ name, path, icon }) => (
-				<li
-					key={name}
-					className="group w-fit"
-				>
-					<Link
-						href={
-							path === "/wishlist" && !user
-								? "/wishlist"
-								: path === "/wishlist" && user
-								? `/wishlist/${user.uid}`
-								: path
-						}
-						className="flex items-center gap-2"
-						onClick={handleNav}
-					>
-						<span
-							className={`${
-								pathname.includes(path)
-									? "text-white"
-									: "text-gray-400 group-hover:text-primaryText duration-300"
-							}`}
-						>
-							{icon}
-						</span>
+  const user = useUser()
+  const pathname = usePathname()
+  return (
+    <ul className="flex flex-col gap-5 mt-5 md:mt-8">
+      {userRoutes.map(({ name, path, icon }) => (
+        <li key={name} className="group w-fit">
+          <Link
+            href={path}
+            // href={
+            // 	path === "/wishlist" && !user
+            // 		? "/wishlist"
+            // 		: path === "/wishlist" && user
+            // 		? `/wishlist/${user.uid}`
+            // 		: path
+            // }
+            className="flex items-center gap-2"
+            onClick={handleNav}
+          >
+            <span
+              className={`${
+                pathname.includes(path)
+                  ? "text-white"
+                  : "text-gray-400 group-hover:text-primaryText duration-300"
+              }`}
+            >
+              {icon}
+            </span>
 
-						{(sidebarStatus || nav) && (
-							<span
-								className={`font-bold md:text-lg text-gray-400 transition-all group-hover:text-primaryText duration-300 ${
-									sidebarStatus || nav ? "block" : "hidden"
-								}`}
-							>
-								{name}
-							</span>
-						)}
-					</Link>
-				</li>
-			))}
-		</ul>
-	)
+            {(sidebarStatus || nav) && (
+              <span
+                className={`font-bold md:text-lg text-gray-400 transition-all group-hover:text-primaryText duration-300 ${
+                  sidebarStatus || nav ? "block" : "hidden"
+                }`}
+              >
+                {name}
+              </span>
+            )}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
 }
